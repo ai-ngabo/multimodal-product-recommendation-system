3714a5a4f4beae6e8ac197350f551bb2e55ad2c2
ai-ngabo <a.ngabo@alustudent.com>
2026-07-17 23:28:18 +0200

    added support configurations for image pipeline

--- src/config.py
+++ src/config.py
@@ -14,3 +14,13 @@ PROC = ROOT / "data" / "cleaned"
 PLOTS = ROOT / "outputs" / "plots"
 FEATURES = ROOT / "outputs" / "features"
 MODELS = ROOT / "outputs" / "models"
+RAW_IMAGES  = ROOT / "data" / "unrefined" / "images"
+FACES = ROOT / "data" / "cleaned" / "faces"
+
+# Biometric Identity Management variables
+USERS = ["alain", "hakim", "joella", "sonia"]
+FACE_EXPRESSION = ["neutral", "smiling", "surprised"]
+PHRASES = {"phrase1": "Yes, approve", "phrase2": "Confirm transaction"} 
+
+# Image settings
+IMG_SIZE = 128
