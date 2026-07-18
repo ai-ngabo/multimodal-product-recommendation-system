e8c20a6496a6ac18704994eb272a89cae7729c7c
ai-ngabo <a.ngabo@alustudent.com>
2026-07-18 02:06:24 +0200

    added settings for the audio pipeline

--- src/config.py
+++ src/config.py
@@ -16,11 +16,18 @@ FEATURES = ROOT / "outputs" / "features"
 MODELS = ROOT / "outputs" / "models"
 RAW_IMAGES  = ROOT / "data" / "unrefined" / "images"
 FACES = ROOT / "data" / "cleaned" / "faces"
+RAW_AUDIO = ROOT / "data" / "unrefined" / "audios"
+CLEAN_AUDIO = ROOT / "data" / "cleaned" / "audios"
 
-# Biometric Identity Management variables
+# Biometric Identity Management variables (FACE + VOICE)
 USERS = ["alain", "hakim", "joella", "sonia"]
 FACE_EXPRESSION = ["neutral", "smiling", "surprised"]
 PHRASES = {"phrase1": "Yes, approve", "phrase2": "Confirm transaction"} 
 
+
 # Image settings
 IMG_SIZE = 128
+
+# Voice Settings
+SR       = 16000
+DURATION = 2.0
